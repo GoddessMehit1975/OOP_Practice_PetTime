50157df042f30f1a35955c919ba6a64efe8166cb
JBROWN4 <juliegbrown1975@gmail.com>
2022-07-27 04:12:57 -0500

    New lesson and self practice with classes

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
-class Dog(object):
-    kind = "canine"
+class Pet:
+    def __init__(self):
+        self.name = input("What is your pet's name?").lower()
+        self.age = input("How old is your pet?").lower()
+        self.color = input("What is your pet's color?").lower()
 
-    def __init__(self, name, age, breed):
-        self.breed = breed
-        self.name = name
-        self.age = age
-        print("This is", self.name, "a", self.breed, "and they are", self.age, "years old")
+    def displaypet(self):
+        self.breed = input("What type is your pet? (Dog, Cat, ...)").lower()
+        print("My pet's name is", self.name, "\n"
+              "They are", self.color, "\n"
+              "They are", self.age,
+              "years old")
+        if self.breed == "Dog":
+            print("Bark Bark")
+        elif self.breed == "Cat":
+            print("Meow Meow")
+        elif self.breed == "Fish":
+            print("Glub Glub")
+        else:
+            print("You own an alien")
 
-    def noise(self):
-        print(self.name, "says 'bark'")
+    def dogspeak(self):
+        print("Bark Bark")
 
-    def sage(self):
-        print("")
-        print(self.name, "says Bark Bark Bark which means 'Hi Katy'")
+    def catspeak(self):
+        print("Meow Meow")
 
-    def persephone(self):
-        print("")
-        print(self.name, "says Bark Bark Bark which means 'Hi Julie'" )
+    def fishspeak(self):
+        print("Glub Glub")
 
-pet1 = Dog("Persephone", 2, "Pitbull")
-pet2 = Dog("Hector", 6, "Cocker Spaniel")
-pet3 = Dog("Betty", 10, "Malamute")
-pet4 = Dog("Sage", 13, "Miniature Schnauzer")
-pet4.sage()
-pet1.persephone()
-print()
+
+pet1 = Pet()
+print("")
+# pet2 = Pet()
+# print("")
+# pet3 = Pet()
+# print("")
+# pet4 = Pet()
+print("")
+pet1.displaypet()
+# print("")
+# pet2.displaypet()
+# print("")
+# pet3.displaypet()
+# print("")
+# pet4.displaypet()
+# print("")
